9fa2f5406f1ae72ff43c95f9e0a1818c92189e05
Frands Otting <frands@otting.dk>
2021-03-20 10:28:10 +0100

    add command to show titles of links in buffer

--- lua/neuron_v2/popup.lua
+++ lua/neuron_v2/popup.lua
@@ -1,58 +1,129 @@
 RELOAD("neuron_v2.helpers")
-local popup = require "popup.init"
+-- local popup = require "popup.init"
 local utils = require "neuron_v2.utils"
 local log = require "neuron_v2.log"
 local helpers = require "neuron_v2.helpers"
+local Job = require("plenary.job")
+local uv = vim.loop
+local co = coroutine
+local a = require("foh.async")
+local neuron_dir = require("neuron_v2.config").neuron_dir.filename
+
 local M = {}
 
 ---Get links from extmark
 ---@param bufnr number
 ---@param extmark_tbl table
-local function get_link_info(bufnr, extmark_tbl)
+local function get_link_info_from_extmarks(bufnr, extmark_tbl)
   bufnr = bufnr or vim.api.nvim_get_current_buf()
+  local links = {}
   for _, line_nr in pairs(vim.tbl_keys(extmark_tbl)) do
-    local links = {}
     -- log.debug(line_nr)
 
     local lines = vim.api.nvim_buf_get_lines(bufnr, line_nr, line_nr + 1, false)
-    log.debug(lines)
+    -- log.debug(lines)
     for _, line in pairs(lines) do
       for link in helpers.link_scanner(line, _, {row = line_nr}) do
         table.insert(links, link)
       end
     end
-    log.debug(links)
   end
-  -- log.debug(row, type(row))
-  -- local find_in_line = vim.api.nvim_buf_get_lines(bufnr, row + 1, false)
-  -- log.debug(tostring(find_in_line[1]))
-  -- local links = helpers.link_scanner(tostring(find_in_line[1]), _)
-  -- for link in links do
-  --   log.debug({link})
-  -- end
-  -- local ext_id = extmark[1]
-  -- local ext_row = extmark[2]
-  -- local ext_col = extmark[3]
-  -- local ext_end_row = extmark[4]["end_row"]
-  -- local ext_end_col = extmark[4]["end_col"]
-  -- log.debug({bufnr, ext_id, ext_row, ext_col, ext_end_row, ext_end_col})
-  -- local line = vim.api.nvim_buf_get_lines(bufnr, ext_row, ext_row + 1, false)
-  -- log.debug(line)
+  -- log.debug(links)
+  return links
 end
 
 function M.show_links(bufnr, ns_id)
   bufnr = bufnr or vim.api.nvim_get_current_buf()
   ns_id = ns_id or vim.api.nvim_create_namespace("neuron_v2")
+
   local all_extmarks = utils.buf_all_extmarks(bufnr, ns_id)
-  -- log.debug(all_extmarks)
+
+  if #all_extmarks == 0 then
+    return P("No popup titles to show")
+  end
+
+  -- make a tbl where each key is a line number with at least 1 extmark
   local extmark_tbl = {}
   for _, e in pairs(all_extmarks) do
+    -- e[2] is the row of the extmark
     extmark_tbl[e[2]] = extmark_tbl[e[2]] or {}
     table.insert(extmark_tbl[e[2]], e)
   end
-  -- log.debug(extmark_tbl)
-  get_link_info(bufnr, extmark_tbl)
+
+  local links_in_buffer = get_link_info_from_extmarks(bufnr, extmark_tbl)
+
+  local query_id = Job
+
+  local jobs = {}
+  local results = {}
+  for _, l in pairs(links_in_buffer) do
+    if l.exists then
+      table.insert(
+        jobs,
+        query_id:new {
+          command = "neuron",
+          args = {
+            "-d",
+            neuron_dir,
+            "query",
+            "--cached",
+            "--id",
+            l.zettelid
+          },
+          on_exit = function(self, _, _)
+            local result = self:result()
+            if #result ~= 1 then
+              table.insert(results, {link = l, result = result})
+            else
+              table.insert(results, {link = l, result = false})
+            end
+            return true
+          end
+        }
+      )
+    end
+  end
+
+  if #jobs > 0 then
+    local status = Job.chain(unpack(jobs))
+    if
+      vim.wait(
+        2000,
+        function()
+          return Job.chain_status(status)
+        end
+      )
+     then
+      for _, result in pairs(results) do
+        if result.result then
+          local zettel_data = vim.fn.json_decode(result.result)
+          local cr = vim.tbl_deep_extend("error", result.link, zettel_data)
+          -- log.debug({result.link, zettel_data})
+          log.debug(cr)
+          require("neuron_v2.popup").create(cr.Title, cr.col, cr.row)
+        else
+          log.debug(result.link .. " doesn't exist")
+        end
+      end
+    end
+  end
 end
 
--- vim.cmd [[nnoremap asdf <cmd>lua require("neuron_v2.popup").show_links()<CR>]]
+function M.create(title, col, row)
+  local bufnr = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_option(bufnr, "bufhidden", "wipe")
+  vim.api.nvim_buf_set_lines(bufnr, 0, -1, true, {title})
+  local opts = {
+    relative = "win",
+    width = string.len(title),
+    height = 1,
+    bufpos = {row, col},
+    anchor = "SW",
+    focusable = false,
+    style = "minimal"
+  }
+  local win_id = vim.api.nvim_open_win(bufnr, false, opts)
+  vim.lsp.util.close_preview_autocmd({"CursorMoved", "CursorMovedI"}, win_id)
+end
+
 return M
