7a10342912b604c00e351f041c7ee71aef265850
Frands Otting <frands@otting.dk>
2021-03-28 23:40:45 +0200

    add reference

---
+++ lua/neuron_v2/queries.lua
@@ -0,0 +1,15 @@
+--[[ 
+
+-- Syntax --> https://neuron.zettel.page/tag-queries
+
+-- List all zettels with tag "walkthrough":
+
+-- [[z:zettels?tag=walkthrough]]
+-- `?grouped`: Group the results by matching tag (use with hierarchical tags)
+-- `?timeline`: Query and display the results as if they form a timeline. This flag,
+-- selects only zettels with a `date` set (see [Zettel metadata](https://neuron.zettel.page/metadata)).
+-- sorts the results by date.
+-- display the date besides the link.
+-- `?showid`: Display the zettel ID alongside the zettel title (link).
+--
+--]]
